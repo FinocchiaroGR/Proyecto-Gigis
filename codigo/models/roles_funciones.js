2682b5cb3f253500f42f7f63ab65ae222f053b1d
FinocchiaroGR <A01704007@itesm.mx>
2021-05-14 06:19:05 -0500

    crearRol para aprobacion

--- codigo/models/roles_funciones.js
+++ codigo/models/roles_funciones.js
@@ -2,20 +2,20 @@ const db = require('../util/database');
 
 module.exports = class Rol_Funcion {
   //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-  constructor(idRol, idFuncion, nombre) {
+  constructor(idRol, idfuncion) {
     this.idRol = idRol;
-    this.idFuncion = idFuncion;
-    this.nombre = nombre;
+    this.idfuncion = idfuncion;
 }
 
   //Este método servirá para guardar de manera persistente el nuevo objeto.
   save() {
     return db.execute(
-      'INSERT INTO roles_funciones (idRol, idFuncion, nombre) VALUES (?,?,?)',
-      [this.idRol,this.idFuncion,this.nombre]
-    );
+      'INSERT INTO `roles_funciones` (`idRol`, `idfuncion`) VALUES (?,?)',
+      [this.idRol,this.idfuncion]
+    ).catch(error => {
+      console.log(error);
+    })
   }
-
   //Este método servirá para devolver los objetos del almacenamiento persistente.
   static fetchAll() {
     return db.execute('SELECT * FROM roles_funciones');
