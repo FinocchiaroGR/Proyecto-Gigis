3bd624ba908b0bb57dd18b0e1d3db8787239035c
tellosss <sandratellosalinas@outlook.es>
2021-05-28 17:17:44 -0500

    Validar que se cuentan con los permisos para las distintas conswultas

--- codigo/controllers/consultas_controller.js
+++ codigo/controllers/consultas_controller.js
@@ -7,83 +7,90 @@ let datosConsultas = new DatosConsultas();
 const arrows = Arrow.fetchAll();
 
 exports.getResultados = ((request, response, next) => {
-    let bools = datosConsultas.getBools();
-    Programas.fetchAll()
-    .then(([rows_Programas, fieldData_Prog]) => {
-        //console.table(rows_Programas);
-        datosConsultas.fetch()
-        .then(([rowsDatos, fieldData_Datos]) => {
-            console.table(rowsDatos);
-            datosConsultas.fetchCants()
-            .then((metaData) => {
-                //console.log(metaData);
-                datosConsultas.fetchGen()
-                .then(([rowsGen, fieldData_Gen]) => {
-                    //console.table(rowsGen);
-                    DatosConsultas.fetchPorGroup_cons()
-                    .then(([rowsGroup, fieldData_Group]) => {
-                        //console.table(rowsGroup);
-                        response.render('consultas_Resultados', {
-                            tituloDeHeader: "Consulta - Resultados",
-                            tituloBarra: "Resultados de consulta",
-                            //metadata
-                            cantProg : metaData.TotProg,
-                            cantCiclos : metaData.TotCicl,
-                            cantCol : metaData.TotCol,
-                            cantPart : metaData.TotPart,
-                            ciclos : {ini : parseInt(metaData.cicloIni),
-                                      fin : parseInt(metaData.cicloFin)},
-                            listaProg : metaData.listaProg,
-                            //bools
-                            estadoConsulta: bools.estadoConsulta,
-                            mostrarSexEdad: bools.mostrarSexEdad,
-                            mostrarCalif: bools.mostrarCalif,
-                            califOava: bools.califOava,
-                            //datos
-                            datos: rowsDatos,
-                            col_Datos: fieldData_Datos,
-                            programas: rows_Programas,
-                            //datos generales
-                            consultaGen: rowsGen,
-                            col_Gen: fieldData_Gen,
-                            //datos por grupos/terapeutas
-                            datosGrupos : rowsGroup,
-                            //utils
-                            backArrow: {display: 'block', link: '/consultas'},
-                            forwArrow: arrows[1]
-                        });
-                        console.log("Consultas Resultados");
-                        response.status(201);
+    const permiso = request.session.permisos;
+    if(permiso.includes(5)){ 
+        let bools = datosConsultas.getBools();
+        Programas.fetchAll()
+        .then(([rows_Programas, fieldData_Prog]) => {
+            //console.table(rows_Programas);
+            datosConsultas.fetch()
+            .then(([rowsDatos, fieldData_Datos]) => {
+                console.table(rowsDatos);
+                datosConsultas.fetchCants()
+                .then((metaData) => {
+                    //console.log(metaData);
+                    datosConsultas.fetchGen()
+                    .then(([rowsGen, fieldData_Gen]) => {
+                        //console.table(rowsGen);
+                        DatosConsultas.fetchPorGroup_cons()
+                        .then(([rowsGroup, fieldData_Group]) => {
+                            //console.table(rowsGroup);
+                            response.render('consultas_Resultados', {
+                                tituloDeHeader: "Consulta - Resultados",
+                                tituloBarra: "Resultados de consulta",
+                                //metadata
+                                cantProg : metaData.TotProg,
+                                cantCiclos : metaData.TotCicl,
+                                cantCol : metaData.TotCol,
+                                cantPart : metaData.TotPart,
+                                ciclos : {ini : parseInt(metaData.cicloIni),
+                                        fin : parseInt(metaData.cicloFin)},
+                                listaProg : metaData.listaProg,
+                                //bools
+                                estadoConsulta: bools.estadoConsulta,
+                                mostrarSexEdad: bools.mostrarSexEdad,
+                                mostrarCalif: bools.mostrarCalif,
+                                califOava: bools.califOava,
+                                //datos
+                                datos: rowsDatos,
+                                col_Datos: fieldData_Datos,
+                                programas: rows_Programas,
+                                //datos generales
+                                consultaGen: rowsGen,
+                                col_Gen: fieldData_Gen,
+                                //datos por grupos/terapeutas
+                                datosGrupos : rowsGroup,
+                                //utils
+                                backArrow: {display: 'block', link: '/consultas'},
+                                forwArrow: arrows[1]
+                            });
+                            console.log("Consultas Resultados");
+                            response.status(201);
+                        }).catch( err => {
+                            request.session.mensaje = 'Error de comunicacion con el servidor1';
+                            request.session.bandera = true;
+                            response.redirect('/consultas');
+                            console.log(err);
+                        })
                     }).catch( err => {
-                        request.session.mensaje = 'Error de comunicacion con el servidor1';
+                        request.session.mensaje = 'Error de comunicacion con el servidor2';
                         request.session.bandera = true;
                         response.redirect('/consultas');
                         console.log(err);
                     })
                 }).catch( err => {
-                    request.session.mensaje = 'Error de comunicacion con el servidor2';
+                    request.session.mensaje = 'Su consulta no arrojó ningun resultado. Por favor ingrese otras condiciones.';
                     request.session.bandera = true;
                     response.redirect('/consultas');
                     console.log(err);
-                })
+                });
             }).catch( err => {
-                request.session.mensaje = 'Su consulta no arrojó ningun resultado. Por favor ingrese otras condiciones.';
+                request.session.mensaje = 'Error de actualizacion de la base de datos';
                 request.session.bandera = true;
                 response.redirect('/consultas');
                 console.log(err);
             });
         }).catch( err => {
-            request.session.mensaje = 'Error de actualizacion de la base de datos';
+            request.session.mensaje = 'Error de comunicacion con el servidor';
             request.session.bandera = true;
             response.redirect('/consultas');
             console.log(err);
         });
-    }).catch( err => {
-        request.session.mensaje = 'Error de comunicacion con el servidor';
-        request.session.bandera = true;
-        response.redirect('/consultas');
-        console.log(err);
-    });
+    }
+    else {
+      response.status(404);
+      response.send('Lo sentimos, este sitio no existe');
+    }
 });
 
 exports.postResultados = ((request, response, next) => {
@@ -97,37 +104,44 @@ exports.getResultadosGrupo = ((request, response, next) => {
     const id = request.params.idGrupo;
     let bools = datosConsultas.getBools();
 
-    datosConsultas.fetchPorGrupo(id)
-    .then(([rows_dato, fieldData_dato]) => {
-        DatosConsultas.DatosGenGrupo(id)
-        .then(([rows_Gen, fieldData_Gen]) => {
-            console.table(rows_Gen);
-            console.table(fieldData_Gen);
-            response.render('consultas_Programa', {
-                tituloDeHeader: 'Resultados ' + rows_Gen.nombrePrograma,
-                tituloBarra: 'Resultados - Programa ' + rows_Gen.idPrograma + ' - Ciclo ' + rows_Gen.idCiclo,
-                mostrarSexEdad: bools.mostrarSexEdad,
-                mostrarCalif: bools.mostrarCalif,
-                col_Datos : fieldData_dato,
-                datos : rows_dato,
-                datoGrupo : rows_Gen,
-                backArrow: {display: 'block', link: '/consultas/Resultados'},
-                forwArrow: arrows[1]
+    const permiso = request.session.permisos;
+    if(permiso.includes(5)){ 
+        datosConsultas.fetchPorGrupo(id)
+        .then(([rows_dato, fieldData_dato]) => {
+            DatosConsultas.DatosGenGrupo(id)
+            .then(([rows_Gen, fieldData_Gen]) => {
+                console.table(rows_Gen);
+                console.table(fieldData_Gen);
+                response.render('consultas_Programa', {
+                    tituloDeHeader: 'Resultados ' + rows_Gen.nombrePrograma,
+                    tituloBarra: 'Resultados - Programa ' + rows_Gen.idPrograma + ' - Ciclo ' + rows_Gen.idCiclo,
+                    mostrarSexEdad: bools.mostrarSexEdad,
+                    mostrarCalif: bools.mostrarCalif,
+                    col_Datos : fieldData_dato,
+                    datos : rows_dato,
+                    datoGrupo : rows_Gen,
+                    backArrow: {display: 'block', link: '/consultas/Resultados'},
+                    forwArrow: arrows[1]
+                });
+                console.log("Consultas Resultados por Grupo");
+                response.status(201);
+            }).catch( err => {
+                request.session.mensaje = 'Error de comunicacion con el servidor';
+                request.session.bandera = true;
+                response.redirect('/consultas');
+                console.log(err);
             });
-            console.log("Consultas Resultados por Grupo");
-            response.status(201);
         }).catch( err => {
             request.session.mensaje = 'Error de comunicacion con el servidor';
             request.session.bandera = true;
             response.redirect('/consultas');
             console.log(err);
         });
-    }).catch( err => {
-        request.session.mensaje = 'Error de comunicacion con el servidor';
-        request.session.bandera = true;
-        response.redirect('/consultas');
-        console.log(err);
-    });
+    }
+    else {
+      response.status(404);
+      response.send('Lo sentimos, este sitio no existe');
+    }
 });
 
 exports.postResultadosGrupo = ((request, response, next) => {
@@ -139,33 +153,42 @@ exports.postResultadosGrupo = ((request, response, next) => {
 exports.getConsultas = ((request, response, next) => {
     const mensaje = request.session.mensaje === undefined ? undefined : request.session.mensaje;
     const bandera = request.session.bandera === undefined ? undefined : request.session.bandera;
-    DatosConsultas.prepConsulta();
+    const permiso = request.session.permisos;
+    const tienePermiso = permiso.includes(5) || permiso.includes(14);
+    if(tienePermiso){     
+        DatosConsultas.prepConsulta();
 
-    Ciclo.fetchFechaCiclo(0)
-    .then(([rows_Fechas, fieldData_Fechas]) => {
-        Ciclo.fetchCantPorAno(0)
-        .then(([rows_CantAno, fieldData_CantAno]) => {
-            Programas.fetchAll()
-            .then(([rows_Programas, fieldData_Prog]) => {
-                response.render('consultas', {
-                    mensaje: mensaje,
-                    bandera: bandera,
-                    tituloDeHeader: "Consultas",
-                    tituloBarra: "Consultas",
-                    años: rows_CantAno,
-                    fechasDeCiclos: rows_Fechas,
-                    programasConsutas: rows_Programas,
-                    numProg: rows_Programas.length,
-                    meses: DatosConsultas.fetchMeses(),
-                    color: DatosConsultas.fetchColors(),
-                    permisos: request.session.permisos,
-                    backArrow: arrows[0],
-                    forwArrow: arrows[1]
+        Ciclo.fetchFechaCiclo(0)
+        .then(([rows_Fechas, fieldData_Fechas]) => {
+            Ciclo.fetchCantPorAno(0)
+            .then(([rows_CantAno, fieldData_CantAno]) => {
+                Programas.fetchAll()
+                .then(([rows_Programas, fieldData_Prog]) => {
+                    response.render('consultas', {
+                        mensaje: mensaje,
+                        bandera: bandera,
+                        tituloDeHeader: "Consultas",
+                        tituloBarra: "Consultas",
+                        años: rows_CantAno,
+                        fechasDeCiclos: rows_Fechas,
+                        programasConsutas: rows_Programas,
+                        numProg: rows_Programas.length,
+                        meses: DatosConsultas.fetchMeses(),
+                        color: DatosConsultas.fetchColors(),
+                        permisos: request.session.permisos,
+                        backArrow: arrows[0],
+                        forwArrow: arrows[1]
+                    });
+                    request.session.mensaje = undefined;
+                    request.session.bandera = undefined;
+                    console.log("Consultas");
+                    response.status(201);
+                }).catch(err => {
+                    request.session.mensaje = 'Error de comunicacion con el servidor';
+                    request.session.bandera = true;
+                    response.redirect('/consultas');
+                    console.log(err);
                 });
-                request.session.mensaje = undefined;
-                request.session.bandera = undefined;
-                console.log("Consultas");
-                response.status(201);
             }).catch(err => {
                 request.session.mensaje = 'Error de comunicacion con el servidor';
                 request.session.bandera = true;
@@ -178,12 +201,11 @@ exports.getConsultas = ((request, response, next) => {
             response.redirect('/consultas');
             console.log(err);
         });
-    }).catch(err => {
-        request.session.mensaje = 'Error de comunicacion con el servidor';
-        request.session.bandera = true;
-        response.redirect('/consultas');
-        console.log(err);
-    });
+    }
+    else {
+      response.status(404);
+      response.send('Lo sentimos, este sitio no existe');
+    }
 });
 
 exports.postConsultas = ((request, response, next) => {
